2a2d03fa7fa8d89e14157b4ccf70d3796eb93208
KostiukIvan <ivan.kostiuk.if@gamil.com>
2020-11-24 21:14:50 +0100

    One diff_pool implementation

--- test.py
+++ test.py
@@ -44,7 +44,7 @@ def load_train_test():
     train_loader = data_utils.DataLoader(MnistBags(target_number=9,
                                                    mean_bag_length=10,
                                                    var_bag_length=2,
-                                                   num_bag=200,
+                                                   num_bag=300,
                                                    seed=1,
                                                    train=True),
                                          batch_size=1,
@@ -53,7 +53,7 @@ def load_train_test():
     test_loader = data_utils.DataLoader(MnistBags(target_number=9,
                                                   mean_bag_length=10,
                                                   var_bag_length=2,
-                                                  num_bag=200,
+                                                  num_bag=100,
                                                   seed=1,
                                                   train=False),
                                         batch_size=1,
@@ -62,7 +62,7 @@ def load_train_test():
     val_loader = data_utils.DataLoader(MnistBags(target_number=9,
                                                   mean_bag_length=10,
                                                   var_bag_length=2,
-                                                  num_bag=100,
+                                                  num_bag=50,
                                                   seed=1,
                                                   train=False),
                                         batch_size=1,
@@ -118,11 +118,11 @@ class Net(torch.nn.Module):
     def __init__(self):
         super(Net, self).__init__()
         self.L = 50
-        self.C_1 = 7
+        self.C_1 = 2
         self.C_2 = 2
         self.classes = 2 # number of classes
         
-        self.n = 3.5 # 0 - no-edges; infinity - fully-conected graph
+        self.n = 5000000 # 0 - no-edges; infinity - fully-conected graph
         self.n_step = 0.5 # inrement n if not enoght items
         self.num_adj_parm = 0.1 # this parameter is used to define min graph adjecment len. num_adj_parm * len(bag). 0 - disable
         
@@ -140,15 +140,14 @@ class Net(torch.nn.Module):
             nn.ReLU(),
         )
 
+        self.gnn1_pool = GNN(self.L, 32, self.C_1, add_loop=True)
+        self.gnn1_embed = GNN(self.L, 24, 2, add_loop=True, lin=False)
 
-        self.gnn1_pool = GNN(50, 32, self.C_1, add_loop=True)
-        self.gnn1_embed = GNN(50, 24, 2, add_loop=True, lin=False)
 
+        self.gnn2_pool = GNN(self.L, 32, self.C_2)
+        self.gnn2_embed = GNN(self.L, 24, 2, lin=False)
 
-        self.gnn2_pool = GNN(50, 32, self.C_2)
-        self.gnn2_embed = GNN(50, 24, 2, lin=False)
-
-        self.gnn3_embed = GNN(50, 32, 32, lin=False)
+        self.gnn3_embed = GNN(self.L, 32, 32, lin=False)
 
         self.lin1 = torch.nn.Linear(3*32, 32)
         self.lin2 = torch.nn.Linear(32, self.classes)
@@ -169,18 +168,20 @@ class Net(torch.nn.Module):
         x = self.gnn1_embed(x, adj)
         
         x, adj, l1, e1 = dense_diff_pool(x, adj, s)
-        
+       
+        '''
         s = self.gnn2_pool(x, adj)
         x = self.gnn2_embed(x, adj)
 
         x, adj, l2, e2 = dense_diff_pool(x, adj, s)
+        '''
         
         x = self.gnn3_embed(x, adj)
 
         x = x.mean(dim=1)
         x = F.relu(self.lin1(x))
         x = self.lin2(x)
-        return F.log_softmax(x, dim=-1), l1 + l2, e1 + e2
+        return F.log_softmax(x, dim=-1), l1 , e1 
     
     # GNN methods
     def convert_bag_to_graph_(self, bag, N):
@@ -193,7 +194,7 @@ class Net(torch.nn.Module):
                     edge_index.append(torch.tensor([cur_i, alt_i]).cuda())
                     
         if len(edge_index) < self.num_adj_parm * bag.shape[0]:
-            #print(f"INFO: get number of adjecment {len(edge_index)}, min len is {self.num_adj_parm * bag.shape[0]}")
+            print(f"INFO: get number of adjecment {len(edge_index)}, min len is {self.num_adj_parm * bag.shape[0]}")
             return self.convert_bag_to_graph_(bag, N = (N + self.n_step))
         
         return bag, torch.stack(edge_index).transpose(1, 0)
@@ -206,7 +207,8 @@ class Net(torch.nn.Module):
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 model = Net().to(device)
-optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
+#optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
+optimizer = torch.optim.Adam(model.parameters(), lr=0.1)
 
 
 def train(epoch):
@@ -220,19 +222,16 @@ def train(epoch):
             data, bag_label = data.cuda(), bag_label.cuda()
         
         data, bag_label = Variable(data), Variable(bag_label)
+            
         optimizer.zero_grad()
-        '''
-        print("1 ", data.x.shape, data.x)
-        print("2 ", data.adj.shape, data.adj)
-        print("3 ", data.mask.shape, data.mask)
-        '''
-        output, _, _ = model(data)
+        output, l, _ = model(data)
+        
         if bag_label:
             target = torch.tensor([1.], dtype=torch.long).cuda()
         else:
             target = torch.tensor([0.], dtype=torch.long).cuda()
         
-        loss = F.nll_loss(output, target)
+        loss = F.nll_loss(output, target) + l
         loss.backward()
         loss_all += target.size(0) * loss.item()
         optimizer.step()
